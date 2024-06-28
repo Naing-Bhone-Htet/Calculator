b30cc4c4c222332557e3ace8ef3eeb96d8a3c955
naing <naing@AUNGMYOSETT>
2024-06-28 19:45:40 +0800

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1;
+            int num2;  
+            Console.WriteLine("Welcome to Calculator Program!");
+            Console.WriteLine("Enter the first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1);
+            Console.WriteLine("Enter the second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num2);
+            Console.ReadLine();
+            
+        }
+    }
+}
