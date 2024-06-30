41b8f3ae6e4bf581e3ebbb5b3e402d764a7931b2
Naing-Bhone-Htet <67512467+Naing-Bhone-Htet@users.noreply.github.com>
2024-06-30 16:02:49 +0800

    Add files via upload

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,14 +5,36 @@
         public static void Main(string[] args)
         {
             int num1;
-            int num2;  
+            int num2;
+            int result;
             Console.WriteLine("Welcome to Calculator Program!");
             Console.WriteLine("Enter the first number: ");
             num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(num1);
             Console.WriteLine("Enter the second number: ");
             num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(num2);
+
+            Console.WriteLine("Enter a for addition, s for subtraction, m for multiplication and anything else for division! ");
+            string operation = Console.ReadLine()!;
+
+            if (operation == "a")
+            {
+                result = num1 + num2;
+            }
+            else if (operation == "b")
+            {
+                result = num1 - num2;
+            }
+            else if (operation == "m")
+            {
+                result = num1 * num2;
+            }
+            else
+            {
+                result = num1 / num2;
+            }
+            Console.WriteLine($"The Result is {result}");
+            Console.WriteLine("Thank You");
+
             Console.ReadLine();
             
         }
